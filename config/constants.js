19911520dc8419d56750ab418e97d28c67fec76f
PiolaEvania <piolaevania@gmail.com>
2025-08-19 09:35:42 +0800

    Menambahkan config constants titik geofencing

---
+++ config/constants.js
@@ -0,0 +1,8 @@
+export const OFFICE_COORDINATES = {
+  latitude: -3.3089332,
+  longitude: 114.613662,
+};
+
+export const GEOFENCE_RADIUS_METERS = 100;
+
+export const TIMEZONE = 'Asia/Makassar'; // WITA
