a55d3ee5bb163ce346f7ac25d9d3374e013675f6
PiolaEvania <piolaevania@gmail.com>
2025-09-28 20:57:08 +0800

    Minor changes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ import exportRoutes from './routes/exportRoutes.js'
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.set('trust proxy', true);
