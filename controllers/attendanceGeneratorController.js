ae87fd5a73e5414f27419cf3356d2212a4572597
PiolaEvania <piolaevania@gmail.com>
2025-09-29 10:44:18 +0800

    Memperbaiki fungsi generate attendance

--- controllers/attendanceGeneratorController.js
+++ controllers/attendanceGeneratorController.js
@@ -32,7 +32,6 @@ export const generateAttendanceRecords = async (req, res) => {
       return res.status(200).json({ message: 'No new attendance records needed for this date range.' });
     }
 
-    await Attendance.deleteMany({ user_id: userId });
     await Attendance.insertMany(recordsToInsert, { ordered: false });
 
     res.status(201).json({
