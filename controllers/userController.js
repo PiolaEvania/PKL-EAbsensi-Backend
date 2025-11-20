a01f4fdfcea8cd852e018a00aebecc5e94bd24eb
PiolaEvania <piolaevania@gmail.com>
2025-11-20 23:12:30 +0800

    Minor fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -148,12 +148,18 @@ export const updateUser = async (req, res) => {
     if (name && !/^[a-zA-Z\s]+$/.test(name)) {
       return res.status(400).json({ message: 'Nama hanya boleh berisi huruf dan spasi.' });
     }
+    if (name.length > 100) {
+      return res.status(400).json({ message: 'Nama maksimal 100 karakter.' });
+    }
 
     if (username) {
       const containsValidChars = /^[a-z0-9]+$/.test(username);
       const containsLetter = /[a-z]/.test(username);
       if (!containsValidChars || !containsLetter) {
         return res.status(400).json({ message: 'Username harus kombinasi huruf atau huruf dengan angka, dan tidak boleh hanya angka.' });
+      }
+      if (username.length > 20) {
+        return res.status(400).json({ message: 'Username maksimal 20 karakter.' });
       }
     }
 
